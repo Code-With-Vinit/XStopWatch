187dd813463284e6ef9d66aa1dedb49db949aa0b
Code-With-Vinit <122153636+Code-With-Vinit@users.noreply.github.com>
2025-06-11 20:10:09 +0530

    Implemented Stopwatch feature

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState,useRef } from 'react'
+
+
+function App() {
+
+  const [time,setTime] = useState(0);
+  const [stopButton,setStopButton] = useState(false);
+
+  const StopwatchRef = useRef(0);
+  const IntervalRef = useRef(null); 
+
+  function handleStart(){ 
+    if(!stopButton)
+    {
+      StopwatchRef.current=new Date().getTime()-time;
+    IntervalRef.current= setInterval(()=>{
+      setTime(new Date().getTime()-StopwatchRef.current);
+    },10);
+    }
+    else
+    {
+      clearInterval(IntervalRef.current);
+    }
+    setStopButton(!stopButton);
+  }
+
+  function handleReset(){
+    clearInterval(IntervalRef.current);
+    setTime(0);
+    setStopButton(false);
+  }
+  function formatTime(){
+
+    // const ms = Math.floor((time%1000)/10);
+    const s = Math.floor((time/1000)%60).toString().padStart(2,"0");
+    const m = Math.floor((time/(1000*60))%60);
+
+    return `${m}:${s}`;
+  }
+  
+
+  return (
+    <>
+      <h1>Stopwatch</h1>
+
+      {<p>Time :  {formatTime()}</p>}
+      <br/>
+      <button onClick={handleStart}>
+        {stopButton ? "Stop" : "Start"}
+      </button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  )
+}
+
+export default App
